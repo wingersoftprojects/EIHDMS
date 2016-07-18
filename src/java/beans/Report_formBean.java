854e124fb443117bc4cc87a2757e4de4ca96fb12
btwesigye <btwesigye@NHQ-LP-002.ura.local>
2016-07-18 21:35:46 +0300

    18 Jul 2016 2

--- src/java/beans/Report_formBean.java
+++ src/java/beans/Report_formBean.java
@@ -5,11 +5,17 @@
  */
 package beans;
 
+import com.sun.javafx.scene.control.skin.VirtualFlow;
 import eihdms.Report_form;
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ManagedProperty;
 import javax.faces.bean.SessionScoped;
+import org.orm.PersistentException;
 
 /**
  *
@@ -18,13 +24,14 @@ import javax.faces.bean.SessionScoped;
 @ManagedBean
 @SessionScoped
 public class Report_formBean extends AbstractBean<Report_form> implements Serializable {
+
     /**
      * Creates a new instance of Report_formBean
      */
     public Report_formBean() {
         super(Report_form.class);
     }
-    
+
     @Override
     public void init() {
         if (super.getEntityClass() == null) {
@@ -40,5 +47,13 @@ public class Report_formBean extends AbstractBean<Report_form> implements Serial
 
     public void setLoginBean(LoginBean loginBean) {
         this.loginBean = loginBean;
+    }
+
+    public List<Report_form> getReport_forms(Report_form report_form) {
+        List<Report_form> report_forms = new ArrayList<>();
+        if (null != report_form) {
+            report_forms.add(report_form);
+        }
+        return report_forms;
     }
 }
