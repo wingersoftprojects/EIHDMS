534322043e7008b57195011b6f0261c50f91b574
bajuna <bajuna@NHQ-LP-157.ura.local>
2018-06-23 13:40:13 +0300

    - Added def_name to report_form_group - Added procedure get_minimum_data_element_id -

--- vpproject/alter_scripts_23.06.2018.sql
+++ vpproject/alter_scripts_23.06.2018.sql
@@ -1,2 +1,4 @@
+update data_element AS d set d.data_element_code=(d.data_element_id-get_minimum_data_element_id(d.report_form_id)+1) where (d.data_element_code is null or d.data_element_code='');
+
 alter table report_form_group add column def_name varchar(100);
 alter table data_element add constraint Report_Form_Group_Data_Element_Code unique (report_form_group_id, data_element_code);
