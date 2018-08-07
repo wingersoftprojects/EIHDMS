7ff0c7dedb686c0cf78668d5b435515b7c325998
bajuna <bajuna@NHQ-LP-157.ura.local>
2018-08-07 19:27:52 +0300

    Added district and entity name on phone contact

--- src/java/beans/Phone_contactBean.java
+++ src/java/beans/Phone_contactBean.java
@@ -6,9 +6,11 @@
 package beans;
 
 import eihdms.Access_scope;
+import eihdms.EIHDMSPersistentManager;
 import eihdms.Organisation;
 import eihdms.Phone_contact;
 import java.io.Serializable;
+import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.faces.application.FacesMessage;
@@ -16,6 +18,7 @@ import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ManagedProperty;
 import javax.faces.bean.SessionScoped;
 import javax.faces.context.FacesContext;
+import org.hibernate.criterion.Restrictions;
 import org.orm.PersistentException;
 
 /**
@@ -60,6 +63,37 @@ public class Phone_contactBean extends AbstractBean<Phone_contact> implements Se
             Logger.getLogger(Phone_contactBean.class.getName()).log(Level.SEVERE, null, ex);
         }
         super.save(aUserDetailId); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    List<Object[]> phone_contact_list;
+    List<Object[]> filtered_phone_contact_list;
+
+    public List<Object[]> getFiltered_phone_contact_list() {
+        return filtered_phone_contact_list;
+    }
+
+    public void setFiltered_phone_contact_list(List<Object[]> filtered_phone_contact_list) {
+        this.filtered_phone_contact_list = filtered_phone_contact_list;
+    }
+
+    public List<Object[]> getPhone_contact_list() {
+        try {
+            phone_contact_list = (List<Object[]>) EIHDMSPersistentManager.instance().getSession().createSQLQuery("select \n"
+                    + "CASE WHEN pc.entity_type='Facility' THEN (SELECT district_name FROM district where district_id in (select district_id from health_facility where health_facility_id=pc.entity_id))\n"
+                    + "\n"
+                    + "WHEN pc.entity_type='Parish' THEN (SELECT district_name FROM district where district_id in (select district_id from county where county_id in (select county_id from sub_county where sub_county_id in (select sub_county_id from parish where parish_id=pc.entity_id))))\n"
+                    + "\n"
+                    + "WHEN pc.entity_type='District' THEN '' END AS District_Name,\n"
+                    + "CASE WHEN pc.entity_type='Facility' THEN (SELECT health_facility_name FROM health_facility where health_facility_id=pc.entity_id) WHEN pc.entity_type='Parish' THEN (SELECT parish_name FROM parish where parish_id=pc.entity_id) WHEN pc.entity_type='District' THEN (SELECT district_name FROM district where district_id=pc.entity_id) END AS Entity_Name, pc.entity_id,pc.entity_type,pc.entity_phone,pc.is_active\n"
+                    + "from phone_contact pc").list();
+        } catch (PersistentException ex) {
+            Logger.getLogger(Phone_contactBean.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return phone_contact_list;
+    }
+
+    public void setPhone_contact_list(List<Object[]> phone_contact_list) {
+        this.phone_contact_list = phone_contact_list;
     }
 
 }
