8865c8387033ac40876de50ac00f0caa6c6e6c11
bajuna <bajuna@NHQ-LP-157.ura.local>
2018-08-08 21:17:03 +0300

    Modified view for Phone contact

--- src/java/beans/Phone_contactBean.java
+++ src/java/beans/Phone_contactBean.java
@@ -5,11 +5,9 @@
  */
 package beans;
 
-import eihdms.Access_scope;
 import eihdms.District;
 import eihdms.EIHDMSPersistentManager;
 import eihdms.Health_facility;
-import eihdms.Organisation;
 import eihdms.Parish;
 import eihdms.Phone_contact;
 import java.io.Serializable;
@@ -22,7 +20,6 @@ import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ManagedProperty;
 import javax.faces.bean.SessionScoped;
 import javax.faces.context.FacesContext;
-import org.hibernate.criterion.Restrictions;
 import org.orm.PersistentException;
 
 /**
@@ -75,6 +72,16 @@ public class Phone_contactBean extends AbstractBean<Phone_contact> implements Se
     List<Object[]> parish_list;
     List<Object[]> district_list;
 
+    Integer adistrict_id;
+
+    public Integer getAdistrict_id() {
+        return adistrict_id;
+    }
+
+    public void setAdistrict_id(Integer adistrict_id) {
+        this.adistrict_id = adistrict_id;
+    }
+
     public List<Object[]> getFiltered_phone_contact_list() {
         return filtered_phone_contact_list;
     }
@@ -92,7 +99,7 @@ public class Phone_contactBean extends AbstractBean<Phone_contact> implements Se
                     + "\n"
                     + "WHEN pc.entity_type='District' THEN '' END AS District_Name,\n"
                     + "CASE WHEN pc.entity_type='Facility' THEN (SELECT health_facility_name FROM health_facility where health_facility_id=pc.entity_id) WHEN pc.entity_type='Parish' THEN (SELECT parish_name FROM parish where parish_id=pc.entity_id) WHEN pc.entity_type='District' THEN (SELECT district_name FROM district where district_id=pc.entity_id) END AS Entity_Name, pc.entity_id,pc.entity_type,pc.entity_phone,pc.is_active\n"
-                    + "from phone_contact pc").list();
+                    + ",phone_contact_id from phone_contact pc").list();
         } catch (PersistentException ex) {
             Logger.getLogger(Phone_contactBean.class.getName()).log(Level.SEVERE, null, ex);
         }
@@ -103,12 +110,16 @@ public class Phone_contactBean extends AbstractBean<Phone_contact> implements Se
         this.phone_contact_list = phone_contact_list;
     }
 
-    public List<Object[]> getFacility_list(District aDistrict) {
-        try {
-            facility_list = (List<Object[]>) EIHDMSPersistentManager.instance().getSession().createSQLQuery("SELECT health_facility_id,health_facility_name"
-                    + " FROM health_facility WHERE district_id='" + aDistrict.getDistrict_id() + "' AND is_active=1");
-        } catch (PersistentException ex) {
-            Logger.getLogger(Phone_contactBean.class.getName()).log(Level.SEVERE, null, ex);
+    public List<Object[]> getFacility_list(Integer aDistrict_id) {
+        if (aDistrict_id != null) {
+            try {
+                facility_list = (List<Object[]>) EIHDMSPersistentManager.instance().getSession().createSQLQuery("SELECT health_facility_id,health_facility_name"
+                        + " FROM health_facility WHERE district_id='" + aDistrict_id + "' AND is_active=1").list();
+            } catch (PersistentException ex) {
+                Logger.getLogger(Phone_contactBean.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        } else {
+            facility_list = new ArrayList<>();
         }
 
         return facility_list;
@@ -118,13 +129,17 @@ public class Phone_contactBean extends AbstractBean<Phone_contact> implements Se
         this.facility_list = facility_list;
     }
 
-    public List<Object[]> getParish_list(District aDistrict) {
-        try {
-            parish_list = (List<Object[]>) EIHDMSPersistentManager.instance().getSession().createSQLQuery("SELECT parish_id,parish_name FROM parish WHERE sub_county_id IN \n"
-                    + "( SELECT sub_county_id FROM sub_county WHERE county_id IN \n"
-                    + "( SELECT county_id FROM county WHERE district_id='" + aDistrict.getDistrict_id() + "'))");
-        } catch (PersistentException ex) {
-            Logger.getLogger(Phone_contactBean.class.getName()).log(Level.SEVERE, null, ex);
+    public List<Object[]> getParish_list(Integer aDistrict_id) {
+        if (aDistrict_id != null) {
+            try {
+                parish_list = (List<Object[]>) EIHDMSPersistentManager.instance().getSession().createSQLQuery("SELECT parish_id,parish_name FROM parish WHERE sub_county_id IN \n"
+                        + "( SELECT sub_county_id FROM sub_county WHERE county_id IN \n"
+                        + "( SELECT county_id FROM county WHERE district_id='" + aDistrict_id + "'))").list();
+            } catch (PersistentException ex) {
+                Logger.getLogger(Phone_contactBean.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        } else {
+            parish_list = new ArrayList<>();
         }
 
         return parish_list;
@@ -146,6 +161,42 @@ public class Phone_contactBean extends AbstractBean<Phone_contact> implements Se
 
     public void setDistrict_list(List<Object[]> district_list) {
         this.district_list = district_list;
+    }
+
+    public void edit_object(Object[] objects) {
+        try {
+            if (objects != null) {
+                Phone_contact phone_contact = Phone_contact.getPhone_contactByORMID((int) objects[6]);
+                if (phone_contact.getEntity_type().equals("Parish")) {
+                    Parish parish = Parish.getParishByORMID(phone_contact.getEntity_id());
+                    adistrict_id = parish.getSub_county().getCounty().getDistrict().getDistrict_id();
+                }
+                if (phone_contact.getEntity_type().equals("Facility")) {
+                    Health_facility health_facility = Health_facility.getHealth_facilityByORMID(phone_contact.getEntity_id());
+                    adistrict_id = health_facility.getDistrict().getDistrict_id();
+                }
+                super.edit(phone_contact);
+            }
+        } catch (Exception ex) {
+            Logger.getLogger(Phone_contactBean.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    public void delete_object(Object[] objects) {
+        try {
+            if (objects != null) {
+                Phone_contact phone_contact = Phone_contact.getPhone_contactByORMID((int) objects[6]);
+                super.delete(phone_contact);
+            }
+        } catch (PersistentException ex) {
+            Logger.getLogger(Phone_contactBean.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    @Override
+    public void cancel() {
+        adistrict_id = null;
+        super.cancel(); //To change body of generated methods, choose Tools | Templates.
     }
 
 }
